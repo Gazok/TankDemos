c0b4712b0e1853f734713638f8c5c5eb7c3a7c55
Gazok <jamiebayne@hotmail.com>
2013-12-13 22:42:57 +0000

    Set second rectangle origin

--- src/RectEnt.cpp
+++ src/RectEnt.cpp
@@ -6,5 +6,6 @@ RectEnt::RectEnt(tank::Vectorf pos)
     : tank::Entity(pos)
 {
     makeGraphic<tank::RectangleShape>(tank::Vectorf{30,30});
-    makeGraphic<tank::RectangleShape>(tank::Vectorf{30,30})->setPos({30,0});
+    auto rectGfx = makeGraphic<tank::RectangleShape>(tank::Vectorf{30,30});
+    rectGfx->setOrigin({-30,0});
 }
