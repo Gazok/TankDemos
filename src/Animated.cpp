2f6ad6dea1095d832826dc15b48a8ac3f53438a3
Gazok <jamiebayne@hotmail.com>
2014-06-21 05:34:30 +0100

    Brought code up to date

--- src/Animated.cpp
+++ src/Animated.cpp
@@ -1,12 +1,16 @@
 #include "Animated.hpp"
 
+#include <chrono>
 #include <Tank/System/Mouse.hpp>
+#include <Tank/System/World.hpp>
+
+using namespace std::literals;
 
 Animated::Animated(tank::Vectorf pos)
     : Entity(pos)
 {
     anim_ = makeGraphic<tank::FrameList>(tank::Image("res/anim.png"), tank::Vectoru{16,16});
-    anim_->add("anim", {0, 1}, 500);
+    anim_->add("anim", {0, 1}, 500ms);
     anim_->select("anim");
     anim_->start();
 }
